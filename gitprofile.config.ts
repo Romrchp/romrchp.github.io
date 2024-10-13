343ff4bac37e2f91e195fd62e52affdf5ea544ce
Romrchp <95102335+Romrchp@users.noreply.github.com>
2024-10-13 22:26:13 +0200

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -20,7 +20,7 @@ const CONFIG = {
         limit: 8, // How many projects to display.
         exclude: {
           forks: true, // Forked projects will not be displayed if set to true.
-          projects: ['romrchp/graded-exs'], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
+          projects: ['romrchp/graded-exs', 'romrchp/optml-project'], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
         },
       },
       manual: {
